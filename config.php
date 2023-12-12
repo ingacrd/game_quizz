1778c98084f29d5cd27bee6ebb0e3eb34ea7ed7f
Carolina Ruiz <ing.acrd@gmail.com>
2023-12-12 14:49:42 -0500

    change database to deploy

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 //Login info 
-define('HOST','localhost');
-define('USER','root');
-define('PASS','');
+define('HOST','dfkpczjgmpvkugnb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306');
+define('USER','w4eyuophd9i9zq74');
+define('PASS','lslal23lvbssqjni');
 
 //DB info 
-define('DBASE', 'kidsGames')
+define('DBASE', 'o6e55f3yywsdx95s')
 
 ?>
