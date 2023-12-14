31b80fe1b7cecb2a5c7461a30fdb51b27513370e
Carolina Ruiz <ing.acrd@gmail.com>
2023-12-14 15:38:50 -0500

    AJAX

--- public/assets/js/signup-onkeyup/uname-ajax.js
+++ public/assets/js/signup-onkeyup/uname-ajax.js
@@ -1,5 +1,6 @@
-const URL = window.location.origin;
-const AJAX_URL = URL + "../../../src/signup-onkeyup/";
+const URL = window.location.origin;//
+//require_once(__DIR__ . '/../config.php');
+const AJAX_URL = URL + "/src/signup-onkeyup/";
 
 function validate_userName() {
     let userName = document.getElementById('userName').value;
